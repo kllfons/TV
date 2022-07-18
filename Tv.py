ce1e2a220accac751ce08b6edc8aace42bad0a19
Kamilla Fonseca <kllfons04@gmail.com>
2022-07-18 02:15:36 -0300

    Update Tv.py

--- Tv.py
+++ Tv.py
@@ -1,26 +1,26 @@
-class Televisão:
+class Tv:
     def __init__(self):
         self.on = False
-        self.canal= 5
+        self.channel= 5
     def power(self):
         if self.on:
             self.on= False
         else :
             self.on = True
-    def aumenta_canal(self):
+    def increase_channel(self):
         if self.on:
-            self. canal+= 1
-    def diminui_canal(self):
+            self. channel+= 1
+    def decrease_channel(self):
         if self.on:
-         self. canal -=1
+         self. channel -=1
 
-televisão= Televisão()
-print ('Televisão está ligada: {}' . format(televisão.on))
-print('Canal:{}'. format(televisão.canal))
-televisão. power()
-print ('Televisão está ligada: {}' . format(televisão.on))
-televisão.aumenta_canal()
-televisão.aumenta_canal()
-print('Canal:{}'. format(televisão.canal))
-televisão.diminui_canal()
-print('Canal:{}'. format(televisão.canal))
+tv= Tv()
+print ('Tv is on: {}' . format(tv.on))
+print('channel:{}'. format(tv.channel))
+tv. power()
+print ('Tv is on: {}' . format(tv.on))
+tv.increase_channel()
+tv.increase_channel()
+print('channel:{}'. format(tv.channel))
+tv.decrease_channel()
+print('channel:{}'. format(tv.channel))
